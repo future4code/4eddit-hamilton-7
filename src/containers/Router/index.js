4c0cb472b42ef2c7302cf90b1bdf5ef5d7dd68af
MarioLuzz <marionetoluz@hotmail.com>
2020-04-29 00:37:19 -0300

    Adicionado home-signup

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
-import LoginPage from "../LoginPage";
-
-
-
+import LoginPage from "../LoginPage/LoginPage";
+import CreateUser from "../CreateUser/CreateUser";
+import Feed from "../Feed/Feed";
 
 export const routes = {
-  root: "/",
   
-  // Outras rotas aqui
+  createUser: "/CreateUser",
+  root: "/",
+  feed: "/feed"
+
 };
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-        <Route path={routes.root} component={LoginPage} />
-        
+        <Route exact path={routes.root} component={LoginPage} />
+        <Route exact path={routes.createUser} component={CreateUser} />
+        <Route exact path={routes.feed} component={Feed} />
       </Switch>
     </ConnectedRouter>
   );
